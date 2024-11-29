28e136e54f538fd5df26c922e3a9f2ed2bc6076d
actualben <actualben@users.noreply.github.com>
2024-11-29 13:29:47 +0100

    more items * update outer compose file with new switchbox image path * starting cdmdb at phase 2 bootstrap to save a little time * misc docs / comments * add cors header for ares app to enable the web worker * using temporary date-specific aresindexer docker tag

--- src/switchbox/__main__.py
+++ src/switchbox/__main__.py
@@ -15,7 +15,7 @@ from .flaskapp import create_app
 def boot_switchbox(logger: logging.Logger, config: Config):
     """
     this function starts switchbox itself from the internal compose file in the
-    subdeployment directory
+    subdeployment directory; this is the 2nd stage of the bootstrap process
     """
     c = Compose(
         project_dir=Path(os.path.join(os.path.dirname(__file__), "subdeployment")),
@@ -23,7 +23,7 @@ def boot_switchbox(logger: logging.Logger, config: Config):
         default_env={"TRAEFIK_PORT": str(config.port)},
     )
 
-    for service in ("ui", "api", "ares"):
+    for service in ("ui", "api", "ares", "cdmdb"):
         logger.info("starting %s", service)
         c.up(service)
     logger.info("background services started; exiting")
@@ -43,12 +43,17 @@ def main() -> int:
     config.logcfg(logger)
 
     if not config.apimode:
-        # start the api and exit
+        # start the other containers and exit
         boot_switchbox(logger, config)
         return 0
-    # if we're still here we're in API mode, which is invoked from the subdeployment directory
+
+    # if we're still here we're in API mode, meaning we've been started from
+    # the subdeployment directory; in API mode we act as a backing service for
+    # the switchbox_ui
 
     app = create_app(config)
+
+    # if you want to change which port the end-users browse to, see config.port
     app.run(host="0.0.0.0", port=8000, debug=config.debug)  # nosec B104
 
     return 0
